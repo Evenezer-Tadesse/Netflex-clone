04764d99a9358d1af7383d5e5a23b382aac31485
KAKA12-19 <evenezertadesse@gmail.com>
2025-07-06 14:58:40 +0300

    some changes in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflex-clone/", // ✅ this is the correct way
+  base: "./", // ✅ for Netlify
   build: {
     outDir: "dist",
     assetsDir: "assets",
